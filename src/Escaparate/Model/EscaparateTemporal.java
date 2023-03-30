d02d0a92477fcaba269b151fce441a96ae7cb82b
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-03-30 14:18:23 +0200

    Escaparate Temporal

---
+++ src/Escaparate/Model/EscaparateTemporal.java
@@ -0,0 +1,33 @@
+package Escaparate.Model;
+
+import Exceptions.ShowRoomNotOpenException;
+import Marca.Marca;
+
+import java.time.LocalDate;
+
+public class EscaparateTemporal extends Escaparate {
+    private int       diasApertura;
+    private LocalDate dateToClose;
+    private LocalDate dateActual;
+
+    public EscaparateTemporal(String nombre, Marca marca, int diasApertura) {
+        super( nombre, marca );
+        this.diasApertura = diasApertura;
+        this.dateToClose  = LocalDate.now().plusDays( diasApertura );
+        this.dateActual   = LocalDate.now();
+    }
+
+    @Override
+    public void sellProduct() {
+        try {
+            if ( this.dateActual.isAfter( this.dateToClose ) ) {
+                throw new ShowRoomNotOpenException();
+            }
+            super.sellProduct();
+        } catch ( ShowRoomNotOpenException e1 ) {
+            System.out.println( e1.getMessage() );
+        }
+    }
+
+    //CurrentTimeMilis
+}
